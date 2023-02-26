ed1790f4bd52b9bf27682ca35432db7a91c9e2d9
rulrurara <110279337+rulrurara@users.noreply.github.com>
2023-02-26 11:44:35 +0900

    [D2] Title: 쉬운 거스름돈, Time: 134 ms, Memory: 20,180 KB -BaekjoonHub

---
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] a = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+		int test = sc.nextInt();
+		for(int t = 1; t <= test; t++) {
+			int n = sc.nextInt();
+			int[] b = new int[a.length];
+			
+			System.out.println("#"+t);
+			
+			for(int i = 0; i < b.length; i++) {
+				b[i] = n / a[i];
+				n = n % a[i];
+				System.out.print(b[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
