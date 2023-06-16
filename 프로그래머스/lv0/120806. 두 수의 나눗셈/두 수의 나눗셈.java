c75401501509ab30f6f668d55f2092a832ca3734
rulrurara <110279337+rulrurara@users.noreply.github.com>
2023-06-16 19:10:48 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        
+       int answer = (int)(((double)num1 / (double)num2) * 1000);
+   
+        return answer;
+        
+    }
+}
